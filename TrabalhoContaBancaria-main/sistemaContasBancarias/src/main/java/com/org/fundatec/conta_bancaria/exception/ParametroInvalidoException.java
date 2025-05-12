67f32bd99243756a338abf6241ad1a3f7eaa7d6a
MatheusRanheri <matheusranheri@gmail.com>
2025-05-12 03:39:50 -0300

    Delete TrabalhoContaBancaria-main/sistemaContasBancarias directory

--- TrabalhoContaBancaria-main/sistemaContasBancarias/src/main/java/com/org/fundatec/conta_bancaria/exception/ParametroInvalidoException.java
+++
@@ -1,8 +0,0 @@
-package com.org.fundatec.conta_bancaria.exception;
-
-public class ParametroInvalidoException extends Exception{
-
-    public ParametroInvalidoException(String mensagem){
-        super(mensagem);
-    }
-}
